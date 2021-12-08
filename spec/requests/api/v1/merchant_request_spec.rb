cf0f1bffcdf79dee1db9de7fa972a8b564f583e8
Lesley Sanders <lesleyasanders@gmail.com>
2021-12-08 12:17:33 -0500

    Add sad path test for find one merchant

---
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "Merchant API endpoints" do
+  it "can get one merchant by its id" do
+    create_list(:merchant, 10)
+
+    get "/api/v1/merchants/#{Merchant.first.id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant[:data][:attributes]).to have_key(:id)
+    expect(merchant[:data][:attributes][:id]).to be_an(Integer)
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
+
+  it 'sends error code if merchant does not exist' do
+    create_list(:merchant, 10)
+
+    get "/api/v1/merchants/100"
+
+    expect(response.status).to eq(404)
+  end
+end
