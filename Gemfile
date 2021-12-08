e3a3eea2ad816e804f625b3eaf8d2aa7c5483e8f
Lesley Sanders <lesleyasanders@gmail.com>
2021-12-08 11:38:46 -0500

    Add faker and factory bot for testing

--- Gemfile
+++ Gemfile
@@ -43,6 +43,9 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'simplecov'
+  gem 'pry'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 group :development do
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
 
 describe "Merchants API" do
   it "sends a list of merchants" do
+    create_list(:merchant, 10)
     get '/api/v1/merchants'
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq(100)
+    expect(merchants[:data].count).to eq(10)
+
+    merchants[:data].each do |merchant|
+      expect(merchant[:attributes]).to have_key(:id)
+      expect(merchant[:attributes][:id]).to be_an(Integer)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
   end
 
   it "can get one merchant by its id" do
