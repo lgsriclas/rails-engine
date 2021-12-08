f13b63b3200f4e0f5db23dedca7bdb39b37b87c4
Lesley Sanders <lesleyasanders@gmail.com>
2021-12-08 16:43:10 -0500

    Add sad path test for create item

--- spec/requests/api/v1/items/item_create_request.rb
+++ spec/requests/api/v1/items/item_create_request.rb
@@ -4,7 +4,7 @@ describe "Item Create API endpoints" do
   it "can create an item" do
     merchant_1 = create(:merchant)
 
-    item_params = ({
+    item_1 = ({
                     name: "Purple Sparkle Leggings",
                     description: "Lounge in style!",
                     unit_price: 20.00,
@@ -13,7 +13,7 @@ describe "Item Create API endpoints" do
 
     header = {"CONTENT_TYPE" => "application/json"}
 
-    post "/api/v1/items/", headers: header, params: JSON.generate(item_params)
+    post "/api/v1/items/", headers: header, params: JSON.generate(item_1)
 
     expect(response).to be_successful
 
@@ -23,5 +23,23 @@ describe "Item Create API endpoints" do
     expect(new_item.name).to eq("Purple Sparkle Leggings")
     expect(new_item.description).to eq("Lounge in style!")
     expect(new_item.unit_price).to eq(20.00)
+    expect(new_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+
+  it "cannot create item if params are missing" do
+    merchant_1 = create(:merchant)
+
+    item_1 = ({
+                    name: "",
+                    description: "Lounge in style!",
+                    unit_price: 20.00,
+                    merchant_id: merchant_1.id
+                  })
+
+    header = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items/", headers: header, params: JSON.generate(item_1)
+
+    expect(response.status).to eq(404)
   end
 end
