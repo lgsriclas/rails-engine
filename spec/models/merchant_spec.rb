d5a3744005691cfa301f3a78a27539772ac401b2
Lesley Sanders <lesleyasanders@gmail.com>
2021-12-14 17:55:26 -0500

    Update model test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,18 +12,18 @@ RSpec.describe Merchant, type: :model do
   describe 'class methods' do
     before :each do
       @merchant = create_list(:merchant, 5)
-      @item_1 = create(:item, merchant: merchant[0])
-      @item_2 = create(:item, merchant: merchant[0])
-      @item_3 = create(:item, merchant: merchant[0])
-      @item_4 = create(:item, merchant: merchant[1])
-      @item_5 = create(:item, merchant: merchant[1])
-      @item_6 = create(:item, merchant: merchant[2])
-      @item_7 = create(:item, merchant: merchant[3])
-      @item_8 = create(:item, merchant: merchant[4])
+      @item_1 = create(:item, merchant: @merchant[0])
+      @item_2 = create(:item, merchant: @merchant[0])
+      @item_3 = create(:item, merchant: @merchant[0])
+      @item_4 = create(:item, merchant: @merchant[1])
+      @item_5 = create(:item, merchant: @merchant[1])
+      @item_6 = create(:item, merchant: @merchant[2])
+      @item_7 = create(:item, merchant: @merchant[3])
+      @item_8 = create(:item, merchant: @merchant[4])
     end
 
     it 'find the top merchants by revenue' do
-      expect(Merchant.top_merchants_by_revenue(3)).to eq([merchant_1, merchant_2, merchant_3])
+      expect(Merchant.top_merchants_by_revenue(3)).to eq([@merchant[0], @merchant[1], @merchant[2]])
     end
   end
 end
